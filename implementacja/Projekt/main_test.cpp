5d76fa3d17e1a9a4976213fde755da7aa376ea87
YK-ov <bubkaogo@gmail.com>
2026-05-27 19:22:18 +0200

    konfiguracja testow poprawiona, usuniety plik TestyJednostkowe.cpp, testy teraz sa wykonywane wylacznie w pliku main_test_.cpp

--- implementacja/Projekt/main_test.cpp
+++ implementacja/Projekt/main_test.cpp
@@ -1,8 +1,31 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+#include "StronaInternetowa.h"
+#include "Student.h"
 
-int main(int argc, char* argv[]) {
-    int result = Catch::Session().run(argc, argv);
+TEST_CASE("Logowanie z poprawnym haslem, musi zalogowac sie") {
+    StronaInternetowa st("adres", "nazwa");
+    Sesja* sesja = new Sesja("151.51.249.120");
+    st.attachSesja(sesja);
+    st.wczytajDaneZPliku("konta.csv", "przedmioty.csv");
+    REQUIRE(st.logowanie("Adriana", "Adriana123", "151.51.249.120") == true);
+
+    delete sesja;
+}
+
+TEST_CASE("Logowanie z blednym haslem, musi nie zalogowac sie") {
+    StronaInternetowa st("adres", "nazwa");
+    Sesja* sesja = new Sesja("151.51.249.120");
+    st.attachSesja(sesja);
+    st.attachSesja(sesja);
+    st.wczytajDaneZPliku("konta.csv", "przedmioty.csv");
+    REQUIRE(st.logowanie("Adriana", "jhgjhg", "151.51.249.120") == false);
+
+    delete sesja;
+}
+
+int main() {
+    int result = Catch::Session().run();
     system("pause");
     return result;
 }
