36c785bb42547eef88fa46b5265043c02782e26b
YK-ov <bubkaogo@gmail.com>
2026-05-27 19:24:59 +0200

    usuniety plik testyJednostkowe.cpp z folderu implementacji

--- implementacja/Projekt/testyJednostkowe.cpp
+++
@@ -1,8 +0,0 @@
-#include "catch.hpp"
-#include "StronaInternetowa.h"
-
-TEST_CASE("Logowanie"){
-    StronaInternetowa st("adres", "nazwa");
-
-    REQUIRE(st.logowanie("Adriana", "Adriana123", "192.220.105.100") == true);
-}
